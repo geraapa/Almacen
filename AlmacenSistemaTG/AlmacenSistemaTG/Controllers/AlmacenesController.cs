7ca7fbb7783ae9343ac9d9bb2c81259700c6e1dd
geraapa <geraapa@gmail.com>
2017-07-22 12:30:35 -0500

    SistemaAlmacenMVC

---
+++ AlmacenSistemaTG/AlmacenSistemaTG/Controllers/AlmacenesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AlmacenSistemaTG.Models;
+
+namespace AlmacenSistemaTG.Controllers
+{
+    public class AlmacenesController : Controller
+    {
+        private SistemaAlmacenEntities db = new SistemaAlmacenEntities();
+
+        // GET: Almacenes
+        public ActionResult Index()
+        {
+            return View(db.Almacens.ToList());
+        }
+
+        // GET: Almacenes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Almacen almacen = db.Almacens.Find(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // GET: Almacenes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Almacenes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "iIdAlmacen,vchDescripcion,vchClave")] Almacen almacen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Almacens.Add(almacen);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(almacen);
+        }
+
+        // GET: Almacenes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Almacen almacen = db.Almacens.Find(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // POST: Almacenes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "iIdAlmacen,vchDescripcion,vchClave")] Almacen almacen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(almacen).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(almacen);
+        }
+
+        // GET: Almacenes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Almacen almacen = db.Almacens.Find(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // POST: Almacenes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Almacen almacen = db.Almacens.Find(id);
+            db.Almacens.Remove(almacen);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
